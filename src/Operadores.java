105f614df3318a26db8685b135b9390a6e244f85
LuisTinoco024 <inghdeztinoco@outlook.es>
2025-10-07 12:35:09 -0600

    feat: Operadores

---
+++ src/Operadores.java
@@ -0,0 +1,26 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Operadores Aritmetico
+        int a = 3, b = 2, resultado = 0;
+
+        //suma
+        resultado = a + b;
+        System.out.println("Suma = " + resultado);
+
+        //resta
+        resultado = a - b;
+        System.out.println("Resta = " + resultado);
+
+        //multiplicacion
+        resultado = a * b;
+        System.out.println("Multiplicacion = " + resultado);
+
+        //division
+        resultado = a / b;
+        System.out.println("Division = " + resultado);
+
+        //modulo
+        resultado = a % b;
+        System.out.println("Modulo = " + resultado);
+    }
+}
---
+++ src/Operadores_Unarios.java
@@ -0,0 +1,11 @@
+public class Operadores_Unarios {
+    public static void main(String[] args) {
+        //Operadors Unarios
+        int a = 3, b = -2, resultado = 0;
+        var c = true;
+
+        //Operador unario -
+         resultado = -a;
+        System.out.println("-a = " + resultado);
+    }
+}
